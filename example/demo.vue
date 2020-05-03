69f424cacb24e6fe15c69d47270283c828cc7144
john015 <lsw0150305@gmail.com>
2020-05-03 18:41:37 +0900

    refactor: change 'VueLoadImage.vue' file code more readable

--- example/demo.vue
+++ example/demo.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <h1>vue-load-image</h1>
-    <button @click="handleChangeImage">change image</button>
+    <button @click="changeImageURL">change image</button>
     <vue-load-image>
-      <img slot="image" :src='src' />
+      <img slot="image" :src="src" />
       <img src="./image-loader.gif" slot="preloader" />
       <div slot="error">image load fails</div>
     </vue-load-image>
@@ -11,29 +11,29 @@
 </template>
 
 <script>
-import VueLoadImage from 'vue-load-image'
+import VueLoadImage from "vue-load-image";
 export default {
-  name: 'App',
-  data () {
+  name: "App",
+  data() {
     return {
-      src: 'https://picsum.photos/500/600/?random'
-    }
+      src: "https://picsum.photos/500/600/?random"
+    };
   },
   methods: {
-    handleChangeImage () {
-      const randomImageNumber = Math.floor(Math.random() * 999) + 1
-      this.src = 'https://picsum.photos/500/600/?image=' + randomImageNumber
+    changeImageURL() {
+      const randomImageNumber = Math.floor(Math.random() * 999) + 1;
+      this.src = "https://picsum.photos/500/600/?image=" + randomImageNumber;
     }
   },
   components: {
-    'vue-load-image': VueLoadImage
+    "vue-load-image": VueLoadImage
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
