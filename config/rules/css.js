0dadf055a78836fdc02bca1194d6339e52f64eb3
john015 <lsw0150305@gmail.com>
2021-06-27 20:07:11 +0900

    feat: Support for Vue 3.x

--- config/rules/css.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  test: /\.css$/,
-  use: [
-    'vue-style-loader',
-    'css-loader'
-  ]
-}
--- src/components/VueLoadImage.vue
+++ src/components/VueLoadImage.vue
@@ -8,75 +8,105 @@
 
 <script>
 const Status = {
-  PENDING: "pending",
-  LOADING: "loading",
-  LOADED: "loaded",
-  FAILED: "failed"
-};
+  PENDING: 'pending',
+  LOADING: 'loading',
+  LOADED: 'loaded',
+  FAILED: 'failed',
+}
 
 export default {
   data() {
     return {
       status: null,
       img: null,
-      src: null
-    };
+      src: null,
+    }
   },
   created() {
-    const { attrs } = this.$slots.image[0].data;
-    this.src = attrs.src || attrs["data-src"];
-    if (this.src) {
-      this.status = Status.LOADING;
-      this.crossOrigin = attrs.crossorigin;
-      this.createLoader();
-      return;
+    const imageElement = this.getImgElementFromImageSlot()
+    if (imageElement == null) {
+      return
     }
-    this.status = Status.PENDING;
+    this.src = imageElement.props.src || imageElement.props['data-src']
+    if (this.src) {
+      this.status = Status.LOADING
+      this.crossOrigin = imageElement.props.crossorigin
+      this.createLoader()
+    } else {
+      this.status = Status.PENDING
+    }
   },
   updated() {
-    const { attrs } = this.$slots.image[0].data;
-    const receivedSrc = attrs.src || attrs["data-src"];
+    const imageElement = this.getImgElementFromImageSlot()
+    if (imageElement == null) {
+      return
+    }
+    const receivedSrc = imageElement.props.src || imageElement.props['data-src']
     if (this.status === Status.LOADING && !this.img) {
-      this.createLoader();
-      return;
+      this.createLoader()
+      return
     }
     if (this.src !== receivedSrc) {
-      this.src = receivedSrc;
-      this.crossOrigin = attrs.crossorigin;
-      this.createLoader();
+      this.src = receivedSrc
+      this.crossOrigin = imageElement.props.crossorigin
+      this.createLoader()
     }
   },
   watch: {
     src(value) {
-      this.status = value ? Status.LOADING : Status.PENDING;
-    }
+      this.status = value ? Status.LOADING : Status.PENDING
+    },
   },
   methods: {
     createLoader() {
-      this.destroyLoader();
-      this.img = new Image();
-      this.img.onload = this.handleLoad;
-      this.img.onerror = this.handleError;
-      this.img.crossOrigin = this.crossOrigin;
-      this.img.src = this.src;
+      this.destroyLoader()
+      this.img = new Image()
+      this.img.onload = this.handleLoad
+      this.img.onerror = this.handleError
+      this.img.crossOrigin = this.crossOrigin
+      this.img.src = this.src
     },
     destroyLoader() {
       if (this.img) {
-        this.img.onload = null;
-        this.img.onerror = null;
-        this.img = null;
+        this.img.onload = null
+        this.img.onerror = null
+        this.img = null
       }
     },
     handleLoad() {
-      this.destroyLoader();
-      this.status = Status.LOADED;
-      this.$emit("onLoad");
+      this.destroyLoader()
+      this.status = Status.LOADED
+      this.$emit('onLoad')
     },
     handleError(error) {
-      this.destroyLoader();
-      this.status = Status.FAILED;
-      this.$emit("onError", error);
-    }
-  }
-};
+      this.destroyLoader()
+      this.status = Status.FAILED
+      this.$emit('onError', error)
+    },
+    getImgElementFromImageSlot() {
+      const imageSlot = this.$slots.image && this.$slots.image()
+      if (imageSlot === undefined) {
+        console.warn('Please define <template v-slot:image> slot in <vue-load-image>')
+        return
+      }
+      return this.findImgElementFrom(imageSlot)
+    },
+    findImgElementFrom(slot) {
+      const queue = [...slot]
+      while (queue.length > 0) {
+        const child = queue.shift()
+        if (
+          child.type === 'img' ||
+          (child.props && child.props['data-src'] != null)
+        ) {
+          return child
+        }
+        if (child.children instanceof Array) {
+          queue.push(...child.children)
+        }
+      }
+      return null
+    },
+  },
+}
 </script>
